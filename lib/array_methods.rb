910fde8dabf3b5da6230d8fc318364053b407ad2
Eliza Munroe <era3munroe@gmail.com>
2019-07-03 17:53:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,10 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ if scale[2] |index|
+
 end
 
 def find_max_value(array)
-  # Add your solution here
+  scale.max
 end
 
 def find_min_value(array)
